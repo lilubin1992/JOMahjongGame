d31a5e8778c3b5364d7bfad4feb2ec49e23d5673
lilubin <lilbuin1992@163.com>
2022-03-20 21:04:14 +0800

    init commit

---
+++ assets/scripts/component/util.ts
@@ -0,0 +1,65 @@
+import { repeat } from "cc";
+
+let handler_pool:handler[] = [];
+let handler_pool_size = 10;
+
+//用于绑定回调函数this指针
+export class handler
+{
+    private cb:Function;
+    private host:any;
+    private args:any[];
+
+    constructor(){}
+
+    init(cb:Function, host = null, ...args)
+    {
+        this.cb = cb;
+        this.host = host;
+        this.args = args;
+    }
+
+    exec(...extras)
+    {
+        this.cb.apply(this.host, this.args.concat(extras));
+    }
+}
+
+export function gen_handler(cb:Function, host:any = null, ...args:any[]):handler
+{
+    let single_handler:handler = handler_pool.length < 0 ? handler_pool.pop(): new handler()
+    //这里要展开args, 否则会将args当数组传给wrapper, 导致其args参数变成2维数组[[]]
+    single_handler.init(cb, host, ...args);
+    return single_handler;
+}
+
+export function strfmt(fmt:string, ...args:any[])
+{
+    return fmt.replace(/\{(\d+)\}/g, (match:string, argIndex:number) => {
+        return args[argIndex] || match;
+    });
+}
+
+export function extend(target, ...sources) {
+    for (var i = 0; i < sources.length; i += 1) {
+        var source = sources[i];
+        for (var key in source) {
+            if (source.hasOwnProperty(key)) {
+                target[key] = source[key];
+            }
+        }
+    }
+    return target;
+}
+
+export function createBreathAction(node:Node, min = 0.9, max = 1.1)
+{
+    
+    // const action = repeatForever(sequence(scaleTo(0.6, max), scaleTo(0.6, min)));
+    // node.runAction(action);
+}
+
+export function destroyBreathAction(node:Node)
+{
+    // node.stopAllActions();
+}
---
+++ assets/scripts/loading.ts
@@ -0,0 +1,59 @@
+
+import { _decorator, Component, Node, Button, Label, log } from 'cc';
+import { HttpService } from './component/httpservice';
+import { gen_handler } from './component/util';
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = loading
+ * DateTime = Wed Mar 16 2022 21:25:09 GMT+0800 (中国标准时间)
+ * Author = 裤子三十八
+ * FileBasename = loading.ts
+ * FileBasenameNoExtension = loading
+ * URL = db://assets/scripts/loading.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/zh/
+ *
+ */
+ 
+@ccclass('loading')
+export class loading extends Component {
+    // [1]
+    // dummy = '';
+
+    // [2]
+    @property(Button)
+    startBtn : Button | null = null;
+
+    host: string = "http://127.0.0.1:3000"
+
+    start () {
+        console.log("hello world");
+        // this.clickBtn()
+    }
+
+    clickBtn() {
+        var lcbel = this.startBtn.node.getChildByName('Label').getComponent(Label);
+        lcbel.string = "hello";
+        log("click btn...");
+        let hand = gen_handler((code, data)=>{
+            log("hello world:"+data.name);
+        }, "http://127.0.0.1:3000");
+        HttpService.getInst().doGet(this.host + '/users', null, null, hand)
+    }
+
+    // update (deltaTime: number) {
+    //     // [4]
+    // }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/zh/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/zh/scripting/decorator.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/zh/scripting/life-cycle-callbacks.html
+ */
